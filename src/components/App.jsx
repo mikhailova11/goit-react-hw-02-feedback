95ad4049d805a9c38b22b1efd27767ccbc0fad5a
Olena <86402764+mikhailova11@users.noreply.github.com>
2022-02-07 23:55:39 +0200

    добавила слушателей и счетчик

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
 import Feedback from "./Feedback";
 
-const  state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-}
-
-
-
-
 export const App = () => {
-
+  const  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
   
   return (
     <div>
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -2,14 +2,35 @@ import React from "react";
 // import s from "./Feedback.module.css";
 
 class Feedback extends React.Component {
-
-      countTotalFeedback() {
-
-      } 
-
-      countPositiveFeedbackPercentage(){
-
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
       }
+    countFeedbackBad = () => {
+        this.setState(( prevState) =>({
+        bad: prevState.bad + 1
+        }))
+    }
+    countFeedbackNeutral = () => {
+        this.setState(( prevState) =>({
+            neutral: prevState.neutral + 1
+        }))
+    }
+    countFeedbackGood = () => {
+    this.setState(( prevState) =>({
+        good: prevState.good + 1
+    }))
+    } 
+
+    countTotalFeedback() {
+        
+
+    } 
+
+    countPositiveFeedbackPercentage(){
+
+    }
 
 
   render() {
@@ -17,15 +38,15 @@ class Feedback extends React.Component {
     <div>
         <h3>Please leave feedback</h3>
         <div>
-            <button>Good</button>
-            <button>Neutral</button>
-            <button>Bad</button>
+            <button type="button" onClick={this.countFeedbackGood}>Good</button>
+            <button type="button" onClick={this.countFeedbackNeutral}>Neutral</button>
+            <button type="button" onClick={this.countFeedbackBad}>Bad</button>
         </div>
         <h3>Statics</h3>
         <div>
-            <span>Good: {}</span>
-            <span>Neutral: {}</span>
-            <span>Bad: {}</span>
+            <span>Good: {this.state.good}</span>
+            <span>Neutral: {this.state.neutral}</span>
+            <span>Bad: {this.state.bad}</span>
             <span>Total: {}</span>
             <span>Positive feedback: {}</span>
         </div>
